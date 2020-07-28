6ae07c8173dacb1738b9f4551b232081f216089b
Beatriz Salles <beatrizsallesss@gmail.com>
2020-07-28 11:57:06 -0300

    Adiçao de pagina de videos e categorias.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <FooterBase>
       <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo DevPrime" />
       </a>
       <p>
         Orgulhosamente criado durante a
